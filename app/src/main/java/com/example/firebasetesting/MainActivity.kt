0938e900775c0ed074fb1de48992d118319e11c6
Vedant Tiwari <vedanttiw168@gmail.com>
2022-03-20 00:01:37 +0530

    added functionality

--- app/src/main/java/com/example/firebasetesting/MainActivity.kt
+++ app/src/main/java/com/example/firebasetesting/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.firebasetesting
 
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.R
 import com.example.firebasetesting.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -30,8 +33,37 @@ class MainActivity : AppCompatActivity() {
         binding.btnLogin.setOnClickListener {
             loginUser()
         }
+
+        binding.btnLogOut.setOnClickListener {
+            logoutUser()
+            checkLoggedInState()
+        }
+        binding.btnUpdate.setOnClickListener {
+            updateProfile()
+        }
     }
 
+    private fun updateProfile(){
+        val user = auth.currentUser
+        user?.let {user ->
+            val userName = binding.username.text.toString()
+            val photoUri = Uri.parse("android.resource://$packageName/${com.example.firebasetesting.R.drawable.ic_profile}")
+            val profileUpdate = UserProfileChangeRequest.Builder().setDisplayName(userName).setPhotoUri(photoUri).build()
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    user.updateProfile(profileUpdate).await()
+                    withContext(Dispatchers.Main){
+                        checkLoggedInState()
+                        Toast.makeText(this@MainActivity,"Successfully Updated user Profile",Toast.LENGTH_LONG).show()
+                    }
+                }catch (e:Exception){
+                    withContext(Dispatchers.Main){
+                        Toast.makeText(this@MainActivity,e.message,Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+    }
 
     private fun registerUser() {
         val email = binding.etEmailRegister.text.toString()
@@ -51,6 +83,10 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    private fun logoutUser(){
+        auth.signOut()
     }
 
     private fun loginUser() {
@@ -74,11 +110,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun checkLoggedInState() {
-        if (auth.currentUser == null){
+        val user = auth.currentUser
+        if (user == null){
             binding.tvLoggedIn.text = "You are not logged in"
         }
         else{
             binding.tvLoggedIn.text = "You are logged in"
+            binding.username.setText(user.displayName)
+            binding.profile.setImageURI(user.photoUrl)
         }
     }
 
